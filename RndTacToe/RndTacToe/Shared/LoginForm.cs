1519eec4406b02449e195a08450f5491698530dd
Nicola Piermattei <pierma.nick@live.it>
2020-11-28 10:41:44 +0100

    Validationhandled

--- RndTacToe/RndTacToe/Shared/LoginForm.cs
+++ RndTacToe/RndTacToe/Shared/LoginForm.cs
@@ -10,9 +10,11 @@ namespace RndTacToe.Shared
     public class LoginForm
     {
         [Required]
+        [Display(Name = "Username")]
         [MinLength(4, ErrorMessage = "Name too short")]
         [MaxLength(16, ErrorMessage = "Name too long")]
         public string Username { get; set; }
+        [Display(Name = "Game Code")]
         public string GameCode { get; set; }
     }
 }
