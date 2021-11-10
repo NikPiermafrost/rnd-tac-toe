0534ed950c96c775b2f76460301b473efcab0ff0
Pierma.Nick <pierma.nick@gmail.com>
2021-11-10 21:54:51 +0100

    lazy loading

---
+++ web/src/app/pages/game/game-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+
+const routes: Routes = [
+  {
+    path: '/:gameId',
+    component: GameComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class GameRoutingModule { }
---
+++ web/src/app/pages/index/index-routing.module.ts
@@ -0,0 +1,16 @@
+import { IndexComponent } from './index/index.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class IndexRoutingModule { }
