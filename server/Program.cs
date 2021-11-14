2100a096e144a3573a3607d39cdcf381b55999e5
Pierma.Nick <pierma.nick@gmail.com>
2021-11-14 15:50:15 +0100

    Already done????

--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,12 @@
+using server.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
+
+app.MapHub<GridHub>("/game-hub");
 
 app.MapGet("/", () => "Hello World!");
 
