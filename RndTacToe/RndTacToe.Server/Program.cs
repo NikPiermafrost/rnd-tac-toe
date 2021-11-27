8c2cea83e4d5693adb9164545b60a3969637c425
NikPiermafrost <pierma.nick@live.it>
2021-11-27 12:41:00 +0100

    Swagger always enabled

--- RndTacToe/RndTacToe.Server/Program.cs
+++ RndTacToe/RndTacToe.Server/Program.cs
@@ -39,11 +39,8 @@ builder.Services.AddSingleton<IHubGroupManager, HubGroupManager>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 if (!app.Environment.IsDevelopment())
 {
