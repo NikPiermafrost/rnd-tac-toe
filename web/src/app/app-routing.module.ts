058839fb3b9fbf8cd1a43b9e7c8bfb2a28dad203
NikPiermafrost <pierma.nick@live.it>
2021-11-30 23:19:09 +0100

    IT FUCKING WORKS

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -40,7 +40,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules
+    preloadingStrategy: PreloadAllModules,
+    useHash: true
   })],
   exports: [RouterModule]
 })
