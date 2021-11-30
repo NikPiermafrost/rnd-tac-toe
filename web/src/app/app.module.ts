020b6691b692577a409bd8061db6007109adeb8c
NikPiermafrost <pierma.nick@live.it>
2021-11-30 19:58:59 +0100

    added new routing strategy

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -8,11 +8,15 @@ import { GameHelperService } from './services/game-helper.service';
 import { IndexComponent } from './pages/index/index.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NewGameComponent } from './pages/index/components/new-game/new-game.component';
+import { JoinGameComponent } from './pages/index/components/join-game/join-game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    NewGameComponent,
+    JoinGameComponent
   ],
   imports: [
     BrowserModule,
