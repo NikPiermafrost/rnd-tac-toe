149699817e7af753e8c8a69a44ec0988cfe7868d
NikPiermafrost <pierma.nick@live.it>
2021-12-13 13:48:52 +0100

    added design

---
+++ rnd-tac-toe-design/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'jit',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        yellow: '#F6C962',
+        blue: '#373B47',
+        white: '#F3F5EF'
+      }      
+    }
+  },
+  plugins: [],
+}
