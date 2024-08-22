73a5a562ac543dde9c62692c7f03515ce0b7d110
Nicola Piermattei <pierma.nick@gmail.com>
2024-08-22 16:42:15 +0200

    webmanifest

--- server/src/plugins/static.ts
+++ server/src/plugins/static.ts
@@ -6,7 +6,6 @@ import fsStatic, { FastifyStaticOptions } from "@fastify/static";
 export default fp<FastifyStaticOptions>(async (fastify, opts) => {
   await fs.promises.mkdir(path.join(__dirname, "../public"), { recursive: true });
   fastify.register(fsStatic, {
-    root: path.join(__dirname, "../public"),
-    etag: true,
+    root: path.join(__dirname, "../public")
   });
 });
