86c4719707dcf8e9d5cb77ecf489860a57a1dbf6
Nicola Piermattei <pierma.nick@gmail.com>
2024-09-20 09:15:42 +0200

    Added rate limiter

---
+++ server/src/plugins/rate-limit.ts
@@ -0,0 +1,17 @@
+import fp from "fastify-plugin";
+import fsRateLimit, { FastifyRateLimitOptions } from "@fastify/rate-limit";
+
+export default fp<FastifyRateLimitOptions>(async (fastify) => {
+  fastify.register(fsRateLimit, {
+    global: true,
+    allowList: ["127.0.0.1"],
+    max: 10000,
+    timeWindow: 1000,
+    addHeaders: {
+      "x-ratelimit-limit": true,
+      "x-ratelimit-remaining": true,
+      "x-ratelimit-reset": true,
+      "retry-after": true
+    }
+  });
+});
